e8f1c61ac995b2c383878d73bdd7a5349562029b
mac <mac@MacBook-Pro.local>
2023-03-02 21:31:46 +0800

    udpate PagingSelect 组件

--- src/views/select/pagingSelect/index.vue
+++ src/views/select/pagingSelect/index.vue
@@ -1,32 +1,55 @@
 <template>
   <div class="paging-select-container">
-    <el-select v-model="select" v-load-more="loadMore" multiple filterable remote reserve-keyword placeholder="请输入搜索关键词"
-      :remote-method="remoteMethod" :loading="loading" clearable>
-      <el-option v-for="item in remoteOptions" :key="item.id" :label="item.name" :value="item.id" />
-    </el-select>
-
+    <paging-select
+      v-model="selectIds"
+      :placeholder="placeholder"
+      :loading="loading"
+      @loadMore="loadMore"
+      @remoteMethod="remoteMethod"
+      style="width: 500px"
+    >
+      <el-option
+        v-for="item in remoteOptions"
+        :key="item.id"
+        :label="`${item.name}-${item.id}`"
+        :value="item.id"
+      />
+    </paging-select>
     <h4>选中的数据 id 列表</h4>
-    <p>{{ selectIds }}</p>
+    <p>{{ selectIds.join(', ') }}</p>
   </div>
 </template>
 
 <script>
 import { getSelectTableList } from '@/api/table'
+import PagingSelect from '@/components/PagingSelect'
 export default {
-  name: 'PagingSelect',
+  name: 'PagingSelectDemo',
+  components: { PagingSelect },
   data() {
     return {
-      page: 1,
-      total: 0,
+      placeholder: '请输入关键字查询',
       loading: false,
-      selectIds: null,
-      remoteOptions: []
+      searchKey: '',
+      selectIds: [],
+
+      remoteOptions: [],
+      page: 1,
+      total: 0
     }
   },
   methods: {
+    // 加载远程数据
     remoteMethod(value) {
-      this.loading = true
+      if (!value) {
+        this.remoteOptions = []
+        this.searchKey = ''
+        return
+      }
+
       this.searchKey = value
+      this.loading = true
+
       const params = {
         page: this.page,
         limit: 10,
@@ -45,6 +68,8 @@ export default {
           this.loading = false
         })
     },
+
+    // 加载更多
     loadMore() {
       if (this.total > this.remoteOptions.length) {
         this.page++
