8e33487dca537891e063c5903dd37dda54a08d82
xzq <ybm4724@dingtalk.com>
2023-04-14 16:58:28 +0800

    add 添加上传用户头像组件

--- src/views/file/uploadAvatar/index.vue
+++ src/views/file/uploadAvatar/index.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="">
-     assasasssasSaasdadasdasds
+    <el-button type="primary" icon="el-icon-upload" style="position: absolute;top: 150px;margin-left: 40px;"
+      @click="imagecropperShow = true">
+      Change Avatar
+    </el-button>
+    <image-cropper v-show="imagecropperShow" :key="imagecropperKey" :width="300" :height="300"
+      url="https://httpbin.org/post" lang-type="zh" @close="close" @crop-upload-success="cropSuccess" />
   </div>
 </template>
 
 <script>
+import ImageCropper from '@/components/ImageCropper'
 export default {
   name: 'UploadAvatar',
-  data () {
+  components: { ImageCropper },
+  data() {
     return {
-      
+      imagecropperShow: false,
+      imagecropperKey: 0,
+      image: 'https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191'
     }
-  },
+  }, methods: {
+    cropSuccess(resData) {
+      this.imagecropperShow = false
+      this.imagecropperKey = this.imagecropperKey + 1
+      this.image = resData.files.avatar
+    },
+    close() {
+      this.imagecropperShow = false
+    }
+  }
 }
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
