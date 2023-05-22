476c5e14e0624d52c9910150f13c862179685f54
mac <mac@MacBook-Pro.local>
2023-05-22 21:49:36 +0800

    update：删除不用的文件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,10 +8,6 @@
       auto-complete="on"
       label-position="left"
     >
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
-      </div>
-
       <el-form-item prop="username">
         <span class="svg-container">
           <svg-icon icon-class="user" />
@@ -46,16 +42,14 @@
           <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
       </el-form-item>
-
       <el-button
         :loading="loading"
         type="primary"
         style="width:100%;margin-bottom:30px;"
         @click.native.prevent="handleLogin"
-      >Login</el-button>
-
+      >登录</el-button>
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
+        <span style="margin-right:20px;">username: admin、editor</span>
         <span>password: any</span>
       </div>
     </el-form>
